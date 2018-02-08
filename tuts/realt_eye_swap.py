ee69795d1d4c5995318dd1d7fad01e0665cc5a85
Avantsa <nagarajitha359@gmail.com>
2018-02-08 17:34:08 +0530

    successfull eye swap

--- tuts/realt_eye_swap.py
+++ tuts/realt_eye_swap.py
@@ -2,6 +2,7 @@
 import numpy as np
 import cv2
 import time
+import copy
 
 def eye_swap(roi_color,eyes):
     if len(eyes)<2:
@@ -20,19 +21,19 @@ def eye_swap(roi_color,eyes):
     eye2_co['y'] = eye2[1]
     eye2_co['w'] = eye2[2]
     eye2_co['h'] = eye2[3]
-
+    temp = copy.copy(roi_color)
     # for (ex, ey, ew, eh) in eyes:
     #    # cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
     #
     tx=0
-    ty=0
     for i in range(eye1_co['x'],eye1_co['x']+eye1_co['w']):
         i2 = eye2_co['x'] + tx
         for j in range(eye1_co['y'],eye1_co['y']+eye1_co['h']):
-            j2=eye2_co['y'] +ty
+            j2=j
             if i2<len(roi_color) and j2<len(roi_color[i2]):
-                roi_color[i][j],roi_color[i2][j2] = roi_color[i2][j2],roi_color[i][j]
-        ty+=1
+                temp[j][i] = roi_color[j][i]
+                roi_color[j][i] = roi_color[j][i2]
+                roi_color[j][i2]=temp[j][i]
         tx+=1
     # for i in range(eye1_co['y'],eye1_co['y']+eye1_co['h']):
     #     j = eye2_co['y']+i
@@ -42,11 +43,30 @@ def eye_swap(roi_color,eyes):
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
+# img = cv2.imread('virat.jpg')536371118856
+
+# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# #cv2.imshow('img', img)
+# faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+# for (x,y,w,h) in faces:
+#     cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+#     roi_gray = gray[y:y+h, x:x+w]
+#     roi_color = img[y:y+h, x:x+w]
+#     eyes = eye_cascade.detectMultiScale(roi_gray)
+#     for (ex,ey,ew,eh) in eyes:
+#         print(ew,eh)
+#         cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+#     roi_color=eye_swap(roi_color,eyes)
+# while(True):
+#     cv2.imshow("frame",img)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+
 cap = cv2.VideoCapture(0)
 while(True):
     # Capture frame-by-frame
     ret, img = cap.read()
-    #img = cv2.imread('slaxk.jpg')
+    #img = cv2.imread('virat.jpg')
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     #cv2.imshow('img', img)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
@@ -55,11 +75,11 @@ while(True):
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
         eyes = eye_cascade.detectMultiScale(roi_gray)
-        for (ex,ey,ew,eh) in eyes:
-            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-        eye_swap(roi_color,eyes)
+        # for (ex,ey,ew,eh) in eyes:
+        #     print(ew,eh)
+        #     cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+        roi_color=eye_swap(roi_color,eyes)
     cv2.imshow('img',img)
-    time.sleep(1)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
